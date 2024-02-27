55d2754887c00f40c12331670b6f4c4a1b21b23f
Noxxider <ravino.juwono@gmail.com>
2024-02-27 15:56:19 -0800

    Add Auto Reset after 2 seconds to Tap BPM

--- src/pages/BPMCounter.vue
+++ src/pages/BPMCounter.vue
@@ -1,10 +1,31 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="q-py-md q-px-xl disable-tap-zoom" style="max-width: 400px;  border: 1px solid #CCCCCC; border-radius: 8px">
-      <div class="text-h6 text-center">Tap to count BPM</div>
-      <div v-if="displayBPM != ''" class="text-h3 text-weight-medium text-center q-mt-md">{{displayBPM}}</div>
-      <div v-if="displayBPM != ''" class="text-h7 text-weight-medium text-center q-mt-xs">Beats/Minute</div>
-      <q-btn @click="calculateBPM" color="accent" label="Tap" no-caps class="q-mt-md q-py-md q-mb-sm full-width" />
+    <div
+      class="q-pt-md q-pb-xs q-px-sm disable-tap-zoom bg-accent"
+      style="min-width: 300px; border: 1px solid #cccccc; border-radius: 8px"
+    >
+      <div class="text-h6 text-center text-white">Tap to count BPM</div>
+      <div
+        v-if="displayBPM != ''"
+        class="text-h3 text-weight-medium text-center q-mt-md text-white"
+      >
+        {{ displayBPM }}
+      </div>
+      <div
+        v-if="displayBPM != ''"
+        class="text-h7 text-weight-medium text-center q-mt-xs text-white"
+      >
+        Beats/Minute
+      </div>
+      <q-btn
+        @click="calculateBPM"
+        :ripple="false"
+        color="accent"
+        label="Tap"
+        no-caps
+        unelevated
+        class="dark-button text-white q-mt-md q-py-md q-mb-sm full-width"
+      />
     </div>
   </q-page>
 </template>
@@ -14,8 +35,8 @@ export default {
   data() {
     return {
       taps: [],
-      displayBPM: '',
-      maxTaps: 8
+      displayBPM: "",
+      maxTaps: 8,
     };
   },
   methods: {
@@ -23,16 +44,22 @@ export default {
       const now = Date.now();
       this.taps.push(now);
 
-      // Keep only the last two taps to calculate the BPM
+      // Keep only the last eight taps to calculate the BPM
       if (this.taps.length > this.maxTaps) {
         this.taps.shift();
       }
 
       if (this.taps.length > 1) {
+        // Auto-reset if more than 2 seconds have passed since the last tap
+        if (now - this.taps[this.taps.length - 2] > 2000) {
+          this.taps = [];
+          return;
+        }
+
         let totalInterval = 0;
         // Calculate the total interval between consecutive taps
         for (let i = 1; i < this.taps.length; i++) {
-          totalInterval += (this.taps[i] - this.taps[i - 1]);
+          totalInterval += this.taps[i] - this.taps[i - 1];
         }
         // Calculate average interval between taps
         const averageInterval = totalInterval / (this.taps.length - 1);
@@ -48,5 +75,10 @@ export default {
 <style scoped>
 .disable-tap-zoom {
   touch-action: manipulation;
+}
+
+.dark-button {
+  background-color: #4f539e !important;
+  border-radius: 8px;
 }
 </style>
